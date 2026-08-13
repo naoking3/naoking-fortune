871f9b1bddfb9ea189a6a239c7ab4d6492e61d79
naoking3 <naonaoru89@gmail.com>
2026-08-14 00:25:56 +0900

    Add files via upload

--- photo-background.js
+++ photo-background.js
@@ -5,15 +5,16 @@ const photoLayers=[document.createElement('i'),document.createElement('i')];
 photoLayers.forEach(layer=>photoBackground?.append(layer));
 function rotateBackground(){
   let next;do{next=Math.floor(Math.random()*backgroundPhotos.length)}while(next===currentPhoto&&backgroundPhotos.length>1);currentPhoto=next;
+  const outgoing=photoLayers.find(layer=>layer.classList.contains('is-visible'));
   const incoming=photoLayers.find(layer=>!layer.classList.contains('is-visible'))||photoLayers[0];
   incoming.style.backgroundImage=`url('${backgroundPhotos[next]}')`;incoming.classList.add('is-visible');
-  photoLayers.filter(layer=>layer!==incoming).forEach(layer=>layer.classList.remove('is-visible'));
+  window.setTimeout(()=>outgoing?.classList.remove('is-visible'),1500);
   const snapshotPhotos=[...document.querySelectorAll('.snapshot-photo')];
   if(snapshotPhotos.length){
     const nextPhoto=snapshotPhotos.find(photo=>!photo.classList.contains('is-visible'))||snapshotPhotos[0];
     nextPhoto.src=backgroundPhotos[next];
-    nextPhoto.onload=()=>nextPhoto.classList.add('is-visible');
-    snapshotPhotos.filter(photo=>photo!==nextPhoto).forEach(photo=>photo.classList.remove('is-visible'));
+    const outgoingPhoto=snapshotPhotos.find(photo=>photo.classList.contains('is-visible'));
+    nextPhoto.onload=()=>{nextPhoto.classList.add('is-visible');window.setTimeout(()=>outgoingPhoto?.classList.remove('is-visible'),1500)};
   }
 }
 rotateBackground();window.setInterval(rotateBackground,8500);
