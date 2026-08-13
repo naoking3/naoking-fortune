3677c7de761eef4487c79af0f2c13fe5de60c707
naoking3 <naonaoru89@gmail.com>
2026-08-14 00:01:24 +0900

    Add files via upload

--- photo-background.js
+++ photo-background.js
@@ -6,9 +6,15 @@ photoLayers.forEach(layer=>photoBackground?.append(layer));
 function rotateBackground(){
   let next;do{next=Math.floor(Math.random()*backgroundPhotos.length)}while(next===currentPhoto&&backgroundPhotos.length>1);currentPhoto=next;
   const incoming=photoLayers.find(layer=>!layer.classList.contains('is-visible'))||photoLayers[0];
-  incoming.style.backgroundImage=`url('${backgroundPhotos[next]}')`;incoming.classList.add('is-visible');
+  incoming.style.backgroundImage=`url('${backgroundPhotos[next]}')`;
+  incoming.style.backgroundPosition=`${30+Math.floor(Math.random()*41)}% ${25+Math.floor(Math.random()*41)}%`;
+  incoming.classList.add('is-visible');
   photoLayers.filter(layer=>layer!==incoming).forEach(layer=>layer.classList.remove('is-visible'));
   const mobilePhoto=document.querySelector('#mobile-photo');
-  if(mobilePhoto)mobilePhoto.src=backgroundPhotos[next];
+  if(mobilePhoto){
+    mobilePhoto.classList.add('is-changing');
+    window.setTimeout(()=>{mobilePhoto.src=backgroundPhotos[next]},700);
+    mobilePhoto.onload=()=>{window.setTimeout(()=>mobilePhoto.classList.remove('is-changing'),80)};
+  }
 }
 rotateBackground();window.setInterval(rotateBackground,8500);
