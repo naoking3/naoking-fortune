4ccb763be79deb1364f885611de6322c0f2fa45a
naoking3 <naonaoru89@gmail.com>
2026-08-13 23:20:21 +0900

    Add files via upload

---
+++ photo-background.js
@@ -0,0 +1,12 @@
+const photoBackground=document.querySelector('#photo-background');
+const backgroundPhotos=['backgrounds/vrchat-01.jpg','backgrounds/vrchat-02.jpg','backgrounds/vrchat-03.jpg','backgrounds/vrchat-04.jpg','backgrounds/vrchat-05.jpg','backgrounds/vrchat-06.jpg','backgrounds/vrchat-07.jpg'];
+let currentPhoto=-1;
+const photoLayers=[document.createElement('i'),document.createElement('i')];
+photoLayers.forEach(layer=>photoBackground?.append(layer));
+function rotateBackground(){
+  let next;do{next=Math.floor(Math.random()*backgroundPhotos.length)}while(next===currentPhoto&&backgroundPhotos.length>1);currentPhoto=next;
+  const incoming=photoLayers.find(layer=>!layer.classList.contains('is-visible'))||photoLayers[0];
+  incoming.style.backgroundImage=`url('${backgroundPhotos[next]}')`;incoming.classList.add('is-visible');
+  photoLayers.filter(layer=>layer!==incoming).forEach(layer=>layer.classList.remove('is-visible'));
+}
+rotateBackground();window.setInterval(rotateBackground,8500);
